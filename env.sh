ee42d2532b526c3285d6f94dcae8ca30dafff1f5
Alexey Kukeev <70845281+MrKooka@users.noreply.github.com>
2021-05-21 12:01:13 +0400

    Delete env.sh

--- env.sh
+++
@@ -1,6 +0,0 @@
-export SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:aezakmiyecgaa4242@mysql:3306/rueng'
-export SECRET_KEY=sdfewr4t56yuikhjmngfbdrew
-export MYSQL_HOST=mysql
-export MYSQL_PASSWORD=aezakmiyecgaa4242
-export MYSQL_DATABASE=rueng
-export MYSQL_ROOT_PASSWORD=aezakmiyecgaa4242
